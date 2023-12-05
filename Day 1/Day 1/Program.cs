9652b1af67555b1a3f986319d9ddb88b7e493f61
DrunkenHyena <62250113+hires-olu@users.noreply.github.com>
2023-12-05 02:41:01 +0000

    Advent Of Code - Day 1
    Written code attempt of Advent of Code Challenge Day 1, written in C#. Part One successfully solved, part two currently being written.

---
+++ Day 1/Day 1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Day_1
+{
+    class Trebuchet
+    {
+        public static int ValueFinderPartOne(StreamReader reader)
+        {
+            int totalCalValues = 0;
+
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                Debug.WriteLine(line);
+
+                string numOnly = Regex.Replace(line, "[^0-9]", "");
+                Debug.WriteLine(numOnly);
+
+                char first_number = numOnly[0];
+                char last_number = numOnly[numOnly.Length - 1];
+
+                string foundValue = first_number.ToString() + last_number.ToString();
+                Debug.WriteLine(foundValue);
+                totalCalValues += int.Parse(foundValue);
+            }
+            reader.Close();
+            return totalCalValues;
+        }
+
+        public static int ValueFinderPartTwo(StreamReader reader)
+        {
+            int totalCalValues = 0;
+            string first_number = null;
+            int firstNumIndex;
+            string last_number = null;
+            int lastNumIndex;
+
+            string[] numbersWritten = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+            string line;
+
+            while (( line = reader.ReadLine()) != null )
+            {
+                Debug.WriteLine(line);
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (Char.IsNumber(line[i])) 
+                    {
+                        if (first_number == null)
+                            first_number = line[i].ToString();
+                        else
+                        {
+
+                            last_number = line[i].ToString();
+                        }
+                    }
+                    else
+                    {
+                        {
+                            string sortingLine = line[i..(line.Length - 1)];
+                        }
+                    }
+                }
+
+
+            }
+            reader.Close();
+            return totalCalValues;
+        }
+        
+
+        public static void Main(string[] args) 
+        {
+            int dayOneAnswerOne;
+            int dayOneAnswerTwo;
+
+            dayOneAnswerOne = ValueFinderPartOne(new StreamReader("E:\\Cloud Storage\\GitHub Works\\Advent-Of-Code-2023-Challenges\\Day 1\\Day 1\\puzzleInput.txt"));
+            Console.WriteLine("Day One Answer Part One: " + dayOneAnswerOne);
+            dayOneAnswerTwo = ValueFinderPartTwo(new StreamReader("E:\\Cloud Storage\\GitHub Works\\Advent-Of-Code-2023-Challenges\\Day 1\\Day 1\\puzzleInput.txt"));
+            Console.WriteLine("Day One Answer Part Two: " + dayOneAnswerTwo);
+        }
+    }
+}
